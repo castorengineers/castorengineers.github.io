b4b6d9184475343320458cbeaeae77f21a02fa45
Jad Ghadry <jadghadry@gmail.com>
2019-04-15 18:19:50 +0300

    initial commit

---
+++ js/jquery.index.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+	// AOS Initialization
+	AOS.init({
+		offset: 140,
+		once: true
+	});
+
+
+
+	// Scroll Spy
+	$('body').scrollspy({
+		target: '.navbar',
+		offset: $('.navbar').outerHeight() + 100
+	});
+
+
+
+	// Collapse Navbar
+	$('.navbar-collapse a').on('click',function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+
+
+	// Smooth Scrolling
+	$(function() {
+		$('a.smooth-scroll').on('click', function(event) {
+			var anchor = $(this).attr('href');
+			var navbarHeight = $('.navbar').outerHeight();
+			$('html, body').stop().animate({
+				scrollTop: $(anchor).offset().top - navbarHeight
+			}, 1000);
+			event.preventDefault();
+		});
+
+	});
+
+
+
+	// Home Typer
+	var homeTyper = new Typed('.home-dynamic-text', {
+		backDelay: 1000,
+		backSpeed: 30,
+		loop: false,
+		smartBackspace: false,
+		strings: ['Castor Engineers.'],
+		typeSpeed: 100
+	});
+
+});
